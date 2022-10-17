92de4956650473a988010ae98b8aedb1987bb93f
Lazar Jankovic <jlazar997@gmail.com>
2022-10-17 19:05:57 +0200

    add ec2 depl

--- 04-mini-project/main.tf
+++ 04-mini-project/main.tf
@@ -39,20 +39,26 @@ variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "avail_zone" {}
 variable "env_prefix" {}
+variable "my_ip" {}
+variable "instance_type" {}
+
+variable "public_key" {}
+// or
+variable "public_key_location" {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
-	 "Name" = "${var.env_prefix}-vpc"
+    "Name" = "${var.env_prefix}-vpc"
   }
 }
 
 resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  cidr_block = var.subnet_cidr_block
+  vpc_id            = aws_vpc.myapp-vpc.id
+  cidr_block        = var.subnet_cidr_block
   availability_zone = var.avail_zone
   tags = {
-	 "Name" = "${var.env_prefix}-subnet"   
+    "Name" = "${var.env_prefix}-subnet"
   }
 }
 
@@ -67,16 +73,156 @@ resource "aws_route_table" "myapp-route-table" {
     "Name" = "${var.env_prefix}-route-table"
   }
 }
+
 # terraform knows in which order resources should be created
 resource "aws_internet_gateway" "myapp-igw" {
   vpc_id = aws_vpc.myapp-vpc.id
-    tags = {
+  tags = {
     "Name" = "${var.env_prefix}-igw"
   }
 }
+
 // if it is not associated, default table = main table
 resource "aws_route_table_association" "a-rtb-subnet" {
-  subnet_id = aws_subnet.myapp-subnet-1.id
+  subnet_id      = aws_subnet.myapp-subnet-1.id
   route_table_id = aws_route_table.myapp-route-table.id
+}
 
+// terraform state show aws_vpc.myapp-vpc -> displays resource in terminal
+// main new route table is created
+resource "aws_default_route_table" "default-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    "Name" = "${var.env_prefix}-main-route-table"
+  }
+}
+
+// ingress for inbound trafic
+// from and to port 22
+// or range of ports i.e. from_port = 0; to_port = 1000
+// cidr_blocks: who is allowed to access
+resource "aws_security_group" "myapp-sg" {
+  name   = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    "Name" = "${var.env_prefix}-sg"
+  }
+}
+
+resource "aws_security_group" "myapp-sg" {
+  name   = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    "Name" = "${var.env_prefix}-sg"
+  }
+}
+
+// Firewall rules:
+// INCOMING (ingress): ssh into ec2 or access from browser
+// OUTGOING (egress): installations & fetching docker images
+
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+  filter {
+    name   = "virtualiyation-type"
+    values = ["hvm"]
+  }
+}
+
+output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-linux-image
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.myapp-server.public_ip
+}
+
+// every user has its own privete key
+resource "aws_key_pair" "ssh-key" {
+  key_name = "server-key"
+  # public_key = "ssh-rsa ********"
+  # public_key = OR USE FILE LOCATION "${file("file location" or var.public_key_location)}"
+  public_key = var.public_key
+}
+
+// localstack create key pair for key_name
+resource "aws_instance" "myapp-server" {
+  ami                    = data.aws_ami.latest-amazon-linux-image.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+  availability_zone      = var.avail_zone
+
+  associate_public_ip_address = true
+
+  key_name = aws_key_pair.ssh-key.key_name
+  // entry point script
+  user_data = file("entry-script.sh")
+  // or
+  # <<EOF
+  #               #!/bin/bash
+  #               sudo yum update -y && sudo yum install -y docker
+  #               sudo systemctl start docker
+  #               sudo usermode -aG docker ec2-user
+  #               docker run -p 8080:80 nginx
+  #             EOF
+
+  tags = {
+    "Name" = "${var.env_prefix}-server"
+  }
 }
