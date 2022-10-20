6d90340f4e052353ccd876114a20f08267ac413e
Lazar Jankovic <jlazar997@gmail.com>
2022-10-20 12:31:53 +0200

    create output file

--- 05-modules/main.tf
+++ 05-modules/main.tf
@@ -184,13 +184,7 @@ data "aws_ami" "latest-amazon-linux-image" {
   }
 }
 
-output "aws_ami_id" {
-  value = data.aws_ami.latest-amazon-linux-image
-}
 
-output "ec2_public_ip" {
-  value = aws_instance.myapp-server.public_ip
-}
 
 // every user has its own privete key
 resource "aws_key_pair" "ssh-key" {
